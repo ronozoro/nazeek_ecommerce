4c2109d9e175b7febcce690a96df5ed43b9166fc
Muhammed Mushtaha <muhamed.mushtaha@gmail.com>
2018-05-31 02:21:17 +0300

    make Carousel responsive

--- nazeek_newReact/src/components/home/newArrivalsSlider/newArrivalsSlider.js
+++ nazeek_newReact/src/components/home/newArrivalsSlider/newArrivalsSlider.js
@@ -11,6 +11,26 @@ const NewArrivalSlider = ({ products }) => {
       id='arrivals-slider'
       dots
       items={4}
+      responsive={{
+        0: {
+          items: 1
+        },
+        470: {
+          items: 2
+        },
+        650: {
+          items: 3
+        },
+        767: {
+          items: 2
+        },
+        991: {
+          items: 3
+        },
+        1199: {
+          items: 4
+        }
+      }}
     >
       {
         data.map((product) => {
